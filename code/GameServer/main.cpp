ad3992536777fbfa602425275cf6208a341b5c57
carrot.qin <382825415@qq.com>
2018-01-27 02:50:05 +0800

    配置CLion工程

---
+++ code/GameServer/main.cpp
@@ -0,0 +1,9 @@
+#include "JB_define.h"
+
+int main()
+{
+    std::cout << "GameServer!\n";
+    return 0;
+}
+
+
---
+++ code/LogServer/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 覃宇骁 on 2018/1/27.
+//
+#include <iostream>
+
+int main()
+{
+    std::cout << "LogServer!\n";
+    return 0;
+}
